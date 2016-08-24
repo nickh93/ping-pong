4d6fe2e6f7d06d97098bedb0e6677211dc2941ee
Nicolas Hurtado <nickh93@outlook.com>
2016-08-24 19:00:25 -0400

    got rid of business and UI logic and got the program to work combining both logics on Scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,72 @@
 $(document).ready(function() {
+  $("form#input").submit(function(event) {
+    event.preventDefault();
 
-  var number;
 
-  var pong = function(number) {
-    var list = [];
-    if (isNaN(number)) {
+    var numberInput = parseInt($("#number").val());
+    $("#result").show;
+    var numList = [];
 
-      alert ("you must enter a valid number");
+    for (var i = 1; i < numberInput; i++) {
 
-    }
-    else {
-
-      for (var index = 1; index <= number; index++) {
-        if (index % 15 === 0) {
-          var pingPong = number[index];
-          pingPong = "Ping-Pong";
-          list.push(pingPong);
-        }
-
-         else {
-         list.push(index);
-       }
-
+      if (i % 15 === 0) {
+        var pingPong = "Ping-Pong";
+        numList.push(pingPong);
       }
+      else if (i % 5 === 0) {
+        var pong = "Pong";
+        numList.push(pong);
+      }
+      else if (i % 3 === 0) {
+        var ping = "Ping";
+        numList.push(ping);
+      }
+      else {
+      numList.push(i);
+      console.log(numList);
+      }
+
+      $("ul#list").append("<li>" + numList[i-1] + "</li>");
+
     }
-  }
+
+    $("#number").val("");
+  });
 });
+
+
+
+
+
+// $(document).ready(function() {
+//
+//   var number = ;
+//
+//   var pong = function(number) {
+//     var list = [];
+//     if (isNaN(number)) {
+//
+//       alert ("you must enter a valid number");
+//
+//     }
+//     else {
+//
+//       for (var index = 1; index <= number; index++) {
+//         if (index % 15 === 0) {
+//           console.log("something is happening");
+//           var pingPong = number[index];
+//           pingPong = "Ping-Pong";
+//           list.push(pingPong);
+//         }
+//
+//          else {
+//          list.push(index);
+//        }
+//        console.log(list);
+//       }
+//     }
+//   }
+// });
 
 //user-interface logic
 // $("#input").submit(function(event) {
